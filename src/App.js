638c4ab009640cbacd94615f544933b44e8665bd
ramaU26 <rama.uarte@gmail.com>
2022-05-31 09:26:27 -0300

    navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
 /*import logo from './logo.svg';*/
 import './App.css';
+import {BrowserRouter as Router, Routes} from 'react-router-dom'
+import Navbar from './components/navegacion/Navbar.js';
+import Inicio from './components/pag/Inicio.js';
+import Productos from './components/pag/Productos.js';
+import Usuario from './components/pag/Usuario.js';
+
+
 
 function App() {
   return (
-   <h1>ARRANCANDO DE 0</h1>
+   <div className='App'>
+     <Router>
+       <Navbar>
+         <Routes>
+           <Router path='/' exact component={Inicio}/>
+           <Router path='/pokemon' exact component={Usuario}/>
+           <Router path='/items' exact component={Productos}/>
+         </Routes>
+       </Navbar>
+     </Router>
+
+
+     </div>
   );
 }
 
---
+++ src/components/pag/Usuario.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Usuario = () =>{
+    return ( 
+        <div>
+        <h1>Usuario</h1>
+
+        </div>
+    )
+}
+
+export default Usuario
